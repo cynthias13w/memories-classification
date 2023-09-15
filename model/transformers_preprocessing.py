64b1de3949ae8953704d6f3ca808c69c751898dc
cynthias13w <mcynsiew@gmail.com>
2023-09-15 13:26:32 +0200

    ✅ Preprocessing V1

--- model/transformers_preprocessing.py
+++ model/transformers_preprocessing.py
@@ -1,20 +1,21 @@
-# installing packages
-# !pip install transforms datasets
-# !pip install evaluate
-# !pip install accelerate -U
-# !pip install transformers[torch]
-from transformers import DistilBertTokenizerFast
+from transformers import AutoTokenizer, DataCollatorWithPadding
+#DistilBertTokenizerFast
+
 # 1. Load Data
-#from datasets import Dataset
+from datasets import Dataset
 from data import load_dataframe
 
 # Firstly load dataframe
-data = load_dataframe()
 
-# load Dataset
-dataset = Dataset.from_pandas(data[['story', 'memType']])
-dataset = dataset.rename_column('memType', 'labels')
-dataset = dataset.rename_column('story', 'text')
+def clean_df():
+    data = load_dataframe()
+
+    # Drop missing values
+    data = data.dropna(subset=['story', 'memType'])
+
+    # load Dataset
+    dataset = Dataset.from_pandas(data[['story', 'memType']])
+    return dataset
 
 # id2label = {0: "imagined", 1: "recalled", 2:"retold"}
 # label2id = {"imagined":0, "recalled": 1, "retold":2}
@@ -25,8 +26,13 @@ def tokenizer_function(dataset):
     Args:
         dataset: Sequence of string to tokenize
     """
-    tokenizer = DistilBertTokenizerFast.from_pretrained("distilbert-base-uncased")
-    return tokenizer(dataset['text'], padding='max_length', truncation=True)
+    tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+    return tokenizer(dataset['story'], padding=True, truncation=True)
 
 def tokenize_data(data):
     return data.map(tokenizer_function, batched = True)
+
+def padding(tokenizer):
+    """Returns data collator object
+    """
+    return DataCollatorWithPadding(tokenizer=tokenizer)
